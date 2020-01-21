d790b8325bdd1ce3347a63b088e306a6d5454212
chloedb <59859078+chloedb@users.noreply.github.com>
2020-01-21 14:26:09 -0800

    Update group randomizer.js

--- group randomizer.js
+++ group randomizer.js
@@ -1,9 +1,7 @@
 const readlineSync = require('readline-sync')
-console.log("hey")
 
 function randomizer() {
-let numberOfGroups = readlineSync.question('How many groups do you want? ');
-let peoplePerGroup = readlineSync.question('How many do you want in each group? ');
+let peoplePerGroup = readlineSync.question('How many people do you want in each group? ');
 }
 
 if (require.main === module) {

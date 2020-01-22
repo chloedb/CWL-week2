fcfbc244b2a51c34ddb44aca2c01e70adb020f29
wismith <56857148+wismith@users.noreply.github.com>
2020-01-22 15:02:44 -0800

    update groupify

--- groupifier/groupify.js
+++ groupifier/groupify.js
@@ -14,26 +14,76 @@ function groupify(randomizedArray, groupSizes){
 function sizeGroups(totalNum, maxSize){
     let smallerSize = maxSize - 1;
     const groupSizes = [];  
+    /*
     if (totalNum % maxSize === 0){
         for (let i = 0; i < totalNum/maxSize; i++){
             groupSizes.push(maxSize);
         }
         return groupSizes;
     }
+    */
+
+    /*
     if (maxSize > Math.ceil(totalNum/2)){
         throw new Error("This group size doesn't give reasonable groups!"); 
     }
+    */
     
+    const works = [];
+    /*
+    for (let i = 1; i <= totalNum/maxSize; i++){
+        workingNum -= maxSize;
+        if (workingNum % smallerSize === 0){
+            works.push(true);
+        } else {
+            works.push(false);
+        }
+    }
+    */
+    let workingNum = totalNum;
+    for (let i = Math.floor(totalNum/maxSize); i > 0; i--){
+        workingNum = totalNum - maxSize*i;
+        if (workingNum % smallerSize === 0){
+            for (let j = 0; j < i; j++){
+                groupSizes.push(maxSize);
+            }
+            while (workingNum > 0){
+                groupSizes.push(smallerSize);
+                workingNum -= smallerSize;
+            }
+            return groupSizes;
+        }
+    }
+    throw new Error("This group size doesn't give reasonable groups!");
 
+
+    /*
+    console.log(works);
+    if (!works.includes(true)){
+        throw new Error("This group size doesn't give reasonable groups!");
+    }
+    workingNum = totalNum;
+    for (let i = 0; i <= works.lastIndexOf(true); i++){
+        groupSizes.push(maxSize);
+        workingNum -= maxSize;
+    }
+    while (workingNum > 0){
+        groupSizes.push(smallerSize);
+        workingNum -= smallerSize;
+    }
+    return groupSizes;    
+    */
+    /*
     while (totalNum >= (maxSize + smallerSize)){
         groupSizes.push(maxSize);
-        totalNum -= maxSize;
+        totalNum -= maxSize;  
     }
     while (totalNum > 0){
         groupSizes.push(smallerSize);
         totalNum -= smallerSize;
     }
     return groupSizes;
+    */
 }
 
 
@@ -48,7 +98,7 @@ let maxSize = randomizer();
 let groupSizes = sizeGroups(totalNum, maxSize);
 let groups = groupify(namesRandom, groupSizes);
 
-console.log("\nGenerating groups...\n");
+console.log(`\nGenerating ${groups.length} random groups...\n`);
 for (let i = 0; i < groups.length; i++) {
     let label = `Group ${i + 1}`;
     console.log(`${label}: ${groups[i]}`);

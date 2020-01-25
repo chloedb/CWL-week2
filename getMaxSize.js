4d2ad36fc2eaedaac7946fbd5185458f28a40bf1
Will Smith <wismith@davidson.edu>
2020-01-24 19:41:45 -0800

    starting new feature

--- getMaxSize.js
+++ getMaxSize.js
@@ -1,5 +1,12 @@
 function getMaxSize() {
-    const readlineSync = require('readline-sync');
+    let readlineSync = require('readline-sync');
+    console.log('  Group by:');
+    console.log('  (1) Number of groups');
+    console.log('  (2) Max group size');
+    let option = readlineSync.questionInt('  Choose an option (1 or 2): ');
+    if (![1,2].includes(option)){
+        
+    }
     let maxGroupSize = Number(readlineSync.question('  Max group size? '));
     return maxGroupSize;
 }
